0f1edad3ab8b8ca5d1cb6c4ef31dc6d7d667307e
MarshallDBrain <marshalldbrain@gmail.com>
2019-03-19 19:56:50 -0400

    Generates a temperature based on a range

--- src/java/com/pulsar/bodys/Body.java
+++ src/java/com/pulsar/bodys/Body.java
@@ -3,13 +3,22 @@ package bodys;
 import java.util.ArrayList;
 import java.util.Map;
 import files.GameFile;
+import math.RanAlg;
+import math.Unit;
 import universe.StarSystem;
 
 public class Body {
 	
-	protected StarSystem starSystem;
+	private Unit temperature;
+	
+	private StarSystem starSystem;
 
 	public Body(Map<String, String> f, StarSystem s, double r) {
+		
+		Unit minT = new Unit(f.get("star_temp.min"));
+		Unit maxT = new Unit(f.get("star_temp.max"));
+		
+		temperature = RanAlg.randomUnit(minT, maxT);
 		
 		starSystem = s;
 		

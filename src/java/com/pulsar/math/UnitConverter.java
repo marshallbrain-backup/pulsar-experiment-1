737f92c796552c593eb1e57f7c658994b2549f0d
MarshallDBrain <marshalldbrain@gmail.com>
2019-03-21 22:55:09 -0400

    End of day commit

--- src/java/com/pulsar/math/UnitConverter.java
+++ src/java/com/pulsar/math/UnitConverter.java
@@ -2,8 +2,25 @@ package math;
 
 public class UnitConverter {
 	
-	public static double convert(double i, UnitType from, UnitType to) {
-		return 0;
+	public static Unit convert(Unit i, UnitType from, UnitType to) {
+		Unit m = convertToMeter(i, from);
+		Unit p = convertFromMeter(m, to);
+		return p;
+		
+	}
+	
+	private static Unit convertToMeter(Unit i, UnitType from) {
+		if(from == UnitType.METER)
+			return i;
+		
+		return null;
+	}
+	
+	private static Unit convertFromMeter(Unit i, UnitType to) {
+		if(to == UnitType.METER)
+			return i;
+		
+		return null;
 	}
 
 }

fbd892b9ffd4972ef7b675a7662af35b48e243ed
MarshallDBrain <marshalldbrain@gmail.com>
2019-03-24 21:58:29 -0400

    Removed the arraylist of stars from a constructor

--- src/java/com/pulsar/universe/StarSystem.java
+++ src/java/com/pulsar/universe/StarSystem.java
@@ -12,12 +12,12 @@ public class StarSystem {
 	
 	private double systemRadius;
 	
-	private ArrayList<Body> starList;
+	private Body star;
+	
 	private ArrayList<Body> planetList;
 	
 	public StarSystem(GameFile gf, Map<String, String> system, Species s) {
 		
-		starList = new ArrayList<Body>();
 		planetList = new ArrayList<Body>();
 		
 		init(gf, system);
@@ -28,8 +28,7 @@ public class StarSystem {
 		
 		Map<String, String> s = gf.getFieldAll(system.get("star") + "\\..*", 1);
 		
-		Body star = new Body(s, this, 0.0);
-		starList.add(star);
+		star = new Body(s, this, 0.0);
 		
 		generatePlanets(gf, star, system.get("num_planets.min"), system.get("num_planets.max"));
 		
@@ -38,13 +37,18 @@ public class StarSystem {
 	private void generatePlanets(GameFile gf, Body star, String min, String max) {
 		
 		int n = RanAlg.randomInt(Integer.parseInt(min), Integer.parseInt(max));
-		double r = RanAlg.randomDouble(0.2, 0.7, 2);
+		double radius = RanAlg.randomDouble(0.2, 0.7, 2);
+		double[] radiusList = new double[n];
 		
 		for(int i = 0; i < n; i++) {
-			planetList.add(new Body(gf, this, star, r, starList));
-			if(systemRadius < r)
-				systemRadius = r;
-			r = RanAlg.randomDouble(r, 0.5, 1.5, 2);
+			radiusList[i] = radius;
+			if(systemRadius < radius)
+				systemRadius = radius;
+			radius = RanAlg.randomDouble(radius, 0.5, 1.5, 2);
+		}
+		
+		for(double r: radiusList) {
+			planetList.add(new Body(gf, this, star, r*149597870700.0, star));
 		}
 		
 	}

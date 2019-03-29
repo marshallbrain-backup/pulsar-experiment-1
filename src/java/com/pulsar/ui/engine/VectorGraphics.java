940c833ac9ec4defdc54db637b4617ae65535a15
MarshallDBrain <marshalldbrain@gmail.com>
2019-03-28 20:17:28 -0400

    Draws filled circle on screen and can translate the graphics object

--- src/java/com/pulsar/ui/engine/VectorGraphics.java
+++ src/java/com/pulsar/ui/engine/VectorGraphics.java
@@ -1,10 +1,38 @@
 package ui.engine;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+
+import pulsar.Main;
 
 public class VectorGraphics {
+	
+	private Graphics2D graphics;
+	private Point screenPosition;
 
 	public VectorGraphics(Graphics g) {
+		graphics = (Graphics2D) g;
+		screenPosition = new Point(0, 0);
+	}
+	
+	public void drawCircle(Circle c) {
+		graphics.fillOval(c.getCenterX()-c.getRadius(), c.getCenterY()-c.getRadius(), c.getRadius()*2, c.getRadius()*2);
+	}
+	
+	public Graphics getGraphics() {
+		return graphics;
+	}
+
+	public void translationSet(ScreenPosition pos) {
+		switch(pos) {
+		case CENTER:
+			Point newPos = new Point(Main.WIDTH/2, Main.HEIGHT/2);
+			graphics.translate(newPos.getX()-screenPosition.getX(), newPos.getY()-screenPosition.getY());
+			break;
+		default:
+			break;
+		}
 	}
 
 }

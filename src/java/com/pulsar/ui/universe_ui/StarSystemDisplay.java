9c72f5516673bfdfd21401f279b9f09c74608dc6
MarshallDBrain <marshalldbrain@gmail.com>
2019-03-29 18:58:35 -0400

    Added render function

--- src/java/com/pulsar/ui/universe_ui/StarSystemDisplay.java
+++ src/java/com/pulsar/ui/universe_ui/StarSystemDisplay.java
@@ -1,5 +1,13 @@
 package ui.universe_ui;
 
+import java.awt.Color;
+import java.util.List;
+import java.util.Map;
+
+import bodys.Body;
+import ui.engine.ScreenPosition;
+import ui.engine.VectorGraphics;
+import ui.engine.VectorParser;
 import universe.StarSystem;
 
 public class StarSystemDisplay {
@@ -8,6 +16,20 @@ public class StarSystemDisplay {
 	
 	public StarSystemDisplay(StarSystem ss) {
 		starSystem = ss;
+		Body s = starSystem.getBodys();
+		List<Map<String, String>> o = getRenderPropertys(s);
+	}
+	
+	public void render(VectorGraphics g) {
+		
+		g.translationSet(ScreenPosition.CENTER);
+		g.getGraphics().setColor(Color.WHITE);
+		
+	}
+
+	private List<Map<String, String>> getRenderPropertys(Body s) {
+		VectorParser.readVectorFile("gfx\\body\\" + s.getType() + ".txt");
+		return null;
 	}
 
 }

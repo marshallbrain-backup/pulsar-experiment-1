bc6a88502963e57e7c0738aa4599b852a5005300
MarshallDBrain <marshalldbrain@gmail.com>
2019-03-28 20:02:14 -0400

    Added centerX, centerY, and radius to constructor

--- src/java/com/pulsar/ui/engine/Circle.java
+++ src/java/com/pulsar/ui/engine/Circle.java
@@ -1,5 +1,27 @@
 package ui.engine;
 
 public class Circle implements Vector {
-
+	
+	private int centerX;
+	private int centerY;
+	private int radius;
+	
+	public Circle(int cx, int cy, int r) {
+		centerX = cx;
+		centerY = cy;
+		radius = r;
+	}
+	
+	public int getCenterX() {
+		return centerX;
+	}
+	
+	public int getCenterY() {
+		return centerY;
+	}
+	
+	public int getRadius() {
+		return radius;
+	}
+	
 }

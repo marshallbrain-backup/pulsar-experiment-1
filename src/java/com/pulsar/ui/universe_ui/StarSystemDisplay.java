2f47e57289925d3d9e000880a1596a0c92d7d8e7
MarshallDBrain <marshalldbrain@gmail.com>
2019-03-28 23:31:30 -0400

    Renamed package

---
+++ src/java/com/pulsar/ui/universe_ui/StarSystemDisplay.java
@@ -0,0 +1,13 @@
+package ui.universe_ui;
+
+import universe.StarSystem;
+
+public class StarSystemDisplay {
+	
+	private StarSystem starSystem;
+	
+	public StarSystemDisplay(StarSystem ss) {
+		starSystem = ss;
+	}
+
+}

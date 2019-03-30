322a7d31d74d0d61dea0ff72983b0899b1e2251c
MarshallDBrain <marshalldbrain@gmail.com>
2019-03-30 09:22:36 -0400

    Made code neater and added a hex pattern

--- src/java/com/pulsar/math/Match.java
+++ src/java/com/pulsar/math/Match.java
@@ -4,15 +4,27 @@ import java.util.regex.Pattern;
 
 public class Match {
 	
-	private static String douPattern = "([0-9]*)\\.([0-9]*)";
-	private static String intPattern = "([0-9]*)";
+	private static String doublePattern = "\\p{Digit}\\.\\p{Digit}";
+	private static String intPattern = "\\p{Digit}";
+	private static String hexPattern = "\\p{XDigit}";
 	
 	public static boolean isDouble(String s) {
-		return (Pattern.matches(douPattern, s) || Pattern.matches(intPattern, s));
+		return (Pattern.matches(doublePattern, s) || isInt(s));
 	}
 	
 	public static boolean isInt(String s) {
-		return Pattern.matches(intPattern, s);
+		return isInt(s, 10);
+	}
+
+	public static boolean isInt(String s, int i) {
+		switch(i) {
+		case 10:
+			return Pattern.matches(intPattern, s);
+		case 16:
+			return Pattern.matches(hexPattern, s);
+		default:
+			return false;
+		}
 	}
 
 }

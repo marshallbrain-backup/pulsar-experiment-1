001cf434a15b7adb29e94f825e45c922f496680e
MarshallDBrain <marshalldbrain@gmail.com>
2019-03-31 22:28:35 -0400

    Renders only the parts of a circle that are visible

--- src/java/com/pulsar/ui/engine/VectorGraphics.java
+++ src/java/com/pulsar/ui/engine/VectorGraphics.java
@@ -4,6 +4,11 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.RenderingHints;
+import java.awt.geom.Arc2D;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Path2D;
+import java.util.ArrayList;
+import java.util.List;
 
 import pulsar.Main;
 
@@ -51,7 +56,79 @@ public class VectorGraphics {
 		int cx = c.getCenterX();
 		int cy = c.getCenterY();
 		int r = Math.toIntExact(c.getRadius());
-		graphics.fillOval(cx-r, cy-r, r*2, r*2);
+//		Arc2D circle = drawVisibleArc(cx, cy, r, -Main.WIDTH/2, -Main.HEIGHT/2, Main.WIDTH/2, Main.HEIGHT/2);
+		List<Arc2D> arcs = drawVisibleArc(cx, cy, 120, -Main.WIDTH/2, -Main.HEIGHT/2, Main.WIDTH/2, Main.HEIGHT/2);
+		
+		Path2D cutCircle = new Path2D.Double();
+		for(Arc2D a: arcs) {
+			cutCircle.append(a, true);
+		}
+		cutCircle.closePath();
+		graphics.fill(cutCircle);
+		
+	}
+	
+	private List<Arc2D> drawVisibleArc(int cx, int cy, int r, int minX, int minY, int maxX, int maxY) {
+		
+		int[] lines = new int[] {maxX, maxY, minX, minY};
+		List<Double> angles = new ArrayList<Double>();
+		List<Arc2D> arcs = new ArrayList<Arc2D>();
+		
+		for(int i = 0; i < lines.length; i++) {
+			
+			int v = lines[i];
+			double z = Math.sqrt(Math.pow(r, 2)-Math.pow(Math.abs(v), 2));
+			
+			if(!Double.isNaN(z)) {
+				
+				double startAngle = 0;
+				double endAngle = 0;
+				
+				if(i%2 == 0) {
+					startAngle = Math.toDegrees(Math.atan2(z, v));
+					endAngle = Math.toDegrees(Math.atan2(-z, v));
+				} else {
+					startAngle = Math.toDegrees(Math.atan2(v, z));
+					endAngle = Math.toDegrees(Math.atan2(v, -z));
+				}
+				
+				angles.add(Math.min(startAngle, endAngle));
+				angles.add(Math.max(startAngle, endAngle));
+				
+			}
+			
+		}
+		
+		for(int i = 1; i < angles.size(); i+=2) {
+			
+			double startAngle = 0;
+			double endAngle = 0;
+			
+			startAngle = angles.get(i);
+			if(i+1 >= angles.size()) {
+				endAngle = angles.get(0);
+			} else {
+				endAngle = angles.get(i+1);
+			}
+			
+			if(endAngle < 0) {
+				endAngle += 360;
+			}
+			
+			endAngle -= startAngle;
+			
+			Arc2D arc = new Arc2D.Double(cx-r, cy-r, r*2, r*2, startAngle, endAngle, Arc2D.OPEN);
+			arcs.add(arc);
+			
+		}
+		
+		if(arcs.isEmpty()) {
+			Arc2D arc = new Arc2D.Double(cx-r, cy-r, r*2, r*2, 0, 360, Arc2D.OPEN);
+			arcs.add(arc);
+		}
+		
+		return arcs;
+		
 	}
 
 }

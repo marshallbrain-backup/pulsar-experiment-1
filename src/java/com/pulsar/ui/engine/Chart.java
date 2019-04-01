a6c7ab7961c5e8463ade2621e36b71d1febbce03
MarshallDBrain <marshalldbrain@gmail.com>
2019-04-01 17:50:34 -0400

    Renamed Chart to UiElement

--- src/java/com/pulsar/ui/engine/Chart.java
+++
@@ -1,9 +0,0 @@
-package ui.map;
-
-import ui.engine.VectorGraphics;
-
-public interface Chart {
-	
-	void render(VectorGraphics g);
-
-}
---
+++ src/java/com/pulsar/ui/engine/UiElement.java
@@ -0,0 +1,7 @@
+package ui.engine;
+
+public interface UiElement {
+	
+	void render(VectorGraphics g);
+
+}

b3ca0941887deb2b5f9ab2394f638c78dbda692a
MarshallDBrain <marshalldbrain@gmail.com>
2019-04-02 11:57:50 -0400

    Added basic functions

--- src/java/com/pulsar/ui/engine/Point.java
+++ src/java/com/pulsar/ui/engine/Point.java
@@ -1,5 +1,44 @@
 package ui.engine;
 
 public class Point {
+	
+	private double x;
+	private double y;
+	
+	public Point() {
+		this(0, 0);
+	}
+	
+	public Point(Point p) {
+		this(p.x, p.y);
+	}
+	
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setLocation(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public void setLocation(java.awt.Point p) {
+		this.x = p.getX();
+		this.y = p.getY();
+	}
+
+	public void move(double dx, double dy) {
+		this.x = x + dx;
+		this.y = y + dy;
+	}
 
 }

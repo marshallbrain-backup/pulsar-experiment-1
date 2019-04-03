99ea360197092b6ed98091c7072b44af0dab1cea
MarshallDBrain <marshalldbrain@gmail.com>
2019-04-02 22:21:09 -0400

    Used Area instead. I AM AN IDIOT!

--- src/java/com/pulsar/ui/engine/VectorGraphics.java
+++ src/java/com/pulsar/ui/engine/VectorGraphics.java
@@ -1,18 +1,10 @@
 package ui.engine;
 
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
-import java.awt.Shape;
-import java.awt.geom.Arc2D;
-import java.awt.geom.Path2D;
-import java.awt.geom.Point2D;
+import java.awt.geom.Area;
 import java.awt.geom.Rectangle2D;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 import pulsar.Main;
 import ui.engine.Point;
 
@@ -28,7 +20,7 @@ public class VectorGraphics {
 		graphicsOriginal = (Graphics2D) g;
 		graphics = (Graphics2D) graphicsOriginal.create();
 		
-		screenBounds = new int[] {-Main.WIDTH/2+200, -Main.HEIGHT/2+200, Main.WIDTH/2-200, Main.HEIGHT/2-200};
+		screenBounds = new int[] {-Main.WIDTH/2, -Main.HEIGHT/2, Main.WIDTH/2, Main.HEIGHT/2};
 		
 		graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 				RenderingHints.VALUE_ANTIALIAS_ON);
@@ -72,142 +64,17 @@ public class VectorGraphics {
 	}
 	
 	public void drawCircle(Circle c, int minX, int minY, int maxX, int maxY) {
+		
 		int cx = c.getCenterX();
 		int cy = c.getCenterY();
 		int r = Math.toIntExact(c.getRadius());
-		List<Shape> arcs = drawVisibleArc(cx, cy, r, minX, minY, maxX, maxY);
 		
-		if(!arcs.isEmpty()) {
-			Path2D cutCircle = new Path2D.Double();
-			for(Shape a: arcs) {
-				cutCircle.append(a, true);
-			}
-			
-			if(arcs.size() == 1) {
-				Arc2D a = ((Arc2D) arcs.get(0));
-				if(a.getStartPoint().getX() != a.getEndPoint().getX() && a.getStartPoint().getY() != a.getEndPoint().getY()) {
-					double x = a.getEndPoint().getX();
-					double y = a.getEndPoint().getY();
-					if(Math.round(x) == minX) {
-						cutCircle.lineTo(minX, maxY);
-					} else if(Math.round(x) == maxX) {
-						cutCircle.lineTo(maxX, minY);
-					} else if(Math.round(y) == minY) {
-						cutCircle.lineTo(minX, minY);
-					} else if(Math.round(y) == maxY) {
-						cutCircle.lineTo(maxX, maxY);
-					}
-				}
-			}
-			
-			cutCircle.closePath();
-			graphics.fill(cutCircle);
-		}
+		Area a = new Area(c.getCircle());
+		Area b = new Area(new Rectangle2D.Double(minX, minY, maxX-minX, maxY-minY));
 		
-		graphics.setColor(Color.BLACK);
-		graphics.drawRect(minX, minY, maxX-minX, maxY-minY);
+		a.intersect(b);
 		
-	}
-	
-	private List<Shape> drawVisibleArc(int cx, int cy, int r, int minX, int minY, int maxX, int maxY) {
-		
-		int[] lines = new int[] {minX-cx, maxX-cx, minY-cy, maxY-cy};
-		List<Double> angles = new ArrayList<Double>();
-		List<Shape> arcs = new ArrayList<Shape>();
-		
-		getAngelIntersection(angles, 0, r, lines[0], lines[2], lines[3]);
-		getAngelIntersection(angles, 1, r, lines[1], lines[2], lines[3]);
-		getAngelIntersection(angles, 2, r, lines[2], lines[0], lines[1]);
-		getAngelIntersection(angles, 3, r, lines[3], lines[0], lines[1]);
-		
-		double totalAngle = 0;
-		
-		for(int i = 0; i < angles.size(); i+=2) {
-			
-			double startAngle = angles.get(i);
-			double endAngle = angles.get(i+1);
-			
-			double offset = endAngle - startAngle;
-			
-			totalAngle += Math.abs(offset);
-			
-		}
-		
-		if(totalAngle > 360) {
-			Collections.sort(angles);
-			angles.set(0, angles.get(0)+360);
-		}
-
-		Collections.sort(angles);
-		
-		for(int i = 0; i < angles.size(); i+=2) {
-			
-			double startAngle = angles.get(i);
-			double endAngle = angles.get(i+1);
-			
-			double offset = endAngle - startAngle;
-			
-			Arc2D arc = new Arc2D.Double(cx-r, cy-r, r*2, r*2, startAngle, offset, Arc2D.OPEN);
-			arcs.add(arc);
-			
-		}
-		
-		if(arcs.isEmpty()) {
-			if((minX < cx-r && cx-r < maxX) && (minY < cy-r && cy-r < maxY)) {
-				Arc2D arc = new Arc2D.Double(cx-r, cy-r, r*2, r*2, 0, 360, Arc2D.OPEN);
-				arcs.add(arc);
-			} else if((minX > cx-r && cx-r > maxX) && (minY > cy-r && cy-r > maxY)) {
-				arcs.add(new Rectangle2D.Float(minX, minY, maxX-minX, maxY-minY));
-			}
-			
-		}
-		
-		return arcs;
-		
-	}
-	
-	private void getAngelIntersection(List<Double> angles, int cor, int r, int l, int min, int max) {
-		
-		double z = Math.sqrt(Math.pow(r, 2)-Math.pow(Math.abs(l), 2));
-		
-		if(!Double.isNaN(z)) {
-			
-			double startAngle = 0;
-			double endAngle = 0;
-			
-			startAngle = Math.toDegrees(Math.atan2(z, l));
-			endAngle = Math.toDegrees(Math.atan2(-z, l));
-			
-			if(cor > 1) {
-				startAngle -= 90;
-				endAngle -= 90;
-				z = -z;
-			}
-			if(cor%2 == 1) {
-				endAngle += 360;
-			}
-			
-			if((startAngle == 0 && endAngle == 0) || (startAngle == 0 && endAngle == 0)) {
-				return;
-			}
-			
-			if(insideBounds(-z, min, max))
-				angles.add(startAngle);
-
-			if(insideBounds(z, min, max))
-				angles.add(endAngle);
-			
-		}
-		
-	}
-
-	private boolean insideBounds(double z, int min, int max) {
-		
-		if(min <= z && z <= max) {
-			return true;
-		}
-		
-		return false;
+		graphics.fill(a);
 		
 	}
 

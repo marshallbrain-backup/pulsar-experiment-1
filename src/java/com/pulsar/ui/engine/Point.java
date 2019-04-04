54f00e5b6e28bdb7dd4ef4f3aa0ff14da13f87cd
MarshallDBrain <marshalldbrain@gmail.com>
2019-04-04 08:40:09 -0400

    Added functions to convert points between absolute and normal

--- src/java/com/pulsar/ui/engine/Point.java
+++ src/java/com/pulsar/ui/engine/Point.java
@@ -2,8 +2,8 @@ package ui.engine;
 
 public class Point {
 	
-	private int x;
-	private int y;
+	private double x;
+	private double y;
 	
 	public Point() {
 		this(0, 0);
@@ -13,20 +13,40 @@ public class Point {
 		this(p.x, p.y);
 	}
 	
-	public Point(int x, int y) {
+	public Point(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
 
-	public int getX() {
+	public Point(Point p, long screenSize, int screenWidth) {
+		this(convert(p.getXLong(), screenSize, screenWidth), convert(p.getYLong(), screenSize, screenWidth));
+	}
+
+	public int getXInt() {
+		return Math.toIntExact(getXLong());
+	}
+
+	public int getYInt() {
+		return Math.toIntExact(getYLong());
+	}
+
+	public long getXLong() {
+		return Math.round(x);
+	}
+
+	public long getYLong() {
+		return Math.round(y);
+	}
+
+	public double getX() {
 		return x;
 	}
 
-	public int getY() {
+	public double getY() {
 		return y;
 	}
 
-	public void setLocation(int x, int y) {
+	public void setLocation(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
@@ -36,9 +56,17 @@ public class Point {
 		this.y = p.y;
 	}
 
-	public void move(int dx, int dy) {
+	public void move(double dx, double dy) {
 		this.x = x + dx;
 		this.y = y + dy;
+	}
+
+	public void move(double dx, double dy, long screenSize, int screenWidth) {
+		move(convert(dx, screenWidth, screenSize), convert(dy, screenWidth, screenSize));
+	}
+	
+	private static long convert(double value, double fromRefrence, double toRefrence) {
+		return Math.round(((value/fromRefrence)*toRefrence));
 	}
 
 }

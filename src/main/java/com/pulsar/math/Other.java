4c3667d83ad6c29ba6a5c5a88dbc251134d36c67
MarshallDBrain <marshalldbrain@gmail.com>
2019-04-08 22:10:02 -0400

    Added getExtension

--- src/main/java/com/pulsar/math/Other.java
+++ src/main/java/com/pulsar/math/Other.java
@@ -1,5 +1,13 @@
 package math;
 
+import java.io.File;
+
 public class Other {
+
+	public static String getExtension(File file) {
+		String name = file.getName();
+		int i = name.lastIndexOf('.');
+		return name.substring(i+1);
+	}
 
 }

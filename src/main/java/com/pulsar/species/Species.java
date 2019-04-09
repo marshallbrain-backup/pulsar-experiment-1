7701bac682132e910a89055543c6ec702ce6c8cd
MarshallDBrain <marshalldbrain@gmail.com>
2019-04-09 09:04:07 -0400

    Clean up

--- src/main/java/com/pulsar/species/Species.java
+++ src/main/java/com/pulsar/species/Species.java
@@ -1,28 +1,15 @@
 package species;
 
-import java.util.ArrayList;
-
-import bodys.Body;
-import bodys.Planet;
 import files.GameFile;
-import files.type.TypeCategory;
-import files.type.TypeLoader;
-import fleets.Ship;
-import species.colony.Colony;
-import species.colony.ResourceManager;
 import species.colony.ResourceManagerMaster;
-import species.colony.Station;
 
 public class Species {
 	
 	private long timeUpdate;
 	private long timePerUpdate;
 	
-	private ArrayList<Body> colonys = new ArrayList<Body>(); //list of colonys that the species controles
-	
 	private GameFile gameFile;
 	private ResourceManagerMaster master; //the master resourcemanager for the entier species
-	private Ship ship;
 	
 	/**
 	 * initalizes species
@@ -36,64 +23,6 @@ public class Species {
 		
 		gameFile = gf;
 		
-		master = new ResourceManagerMaster(this, gf);
-		
-		ship = new Ship(this);
-		
-	}
-	
-	/**
-	 * creates a colony for this species
-	 * 
-	 * @param planet the planet that the colony is on
-	 * @return colony the colony that was created
-	 */
-	public Colony createColony(Body b) {
-		
-		if(b.getColonyType() == null)
-			return null;
-		
-		Colony c = new Colony(b, this, speciesLoader);
-		
-		b.addColony(c);
-		colonys.add(b);
-		ship.setSystem(b.getSystem());
-		
-		return c;
-		
-	}
-
-	public Station createStation(Body b) {
-		
-		if(!b.tryAddStation()) {
-			return null;
-		}
-		
-		Colony c = b.getColony();
-		Station s = null;
-		
-		if(c == null) {
-			s = new Station(b, this, speciesLoader);
-		} else {
-			s = new Station(b, speciesLoader);
-		}
-		
-		if(s != null) {
-			b.addStation(s);
-			colonys.add(b);
-		}
-		
-		return s;
-		
-	}
-	
-	/**
-	 * adds a resourcemanager to the speses. alows updating all resorses through one class
-	 * 
-	 * @param resourcemanager the resorse manager of a colony to add to the speses
-	 */
-	public void add(ResourceManager r) {
-		master.add(r);
 	}
 	
 	/**
@@ -106,21 +35,10 @@ public class Species {
 		if(timeUpdate >= timePerUpdate) {
 			
 			int updateNum = (int) Math.floorDiv(timeUpdate, timePerUpdate);
-			timeUpdate = 0;
-			master.update(updateNum);
+			timeUpdate -= timePerUpdate;
 			
 		}
 		
-		ship.update(update);
-		
-	}
-
-	public TypeCategory getDefaltPopCategory() {
-		return master.getDefaltPopCategory();
-	}
-
-	public ResourceManagerMaster getResourceManagerMaster() {
-		return master;
 	}
 
 }

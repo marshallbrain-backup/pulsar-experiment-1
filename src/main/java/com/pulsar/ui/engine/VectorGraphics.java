eb5dec2657126ecf8330b2d014e99d51d52e1c1b
MarshallDBrain <marshalldbrain@gmail.com>
2019-04-11 18:26:44 -0400

    Draw handles both draw and fill functions

--- src/main/java/com/pulsar/ui/engine/VectorGraphics.java
+++ src/main/java/com/pulsar/ui/engine/VectorGraphics.java
@@ -1,11 +1,14 @@
 package ui.engine;
 
+import java.awt.BasicStroke;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.geom.Area;
 import java.awt.geom.NoninvertibleTransformException;
 import java.awt.geom.Rectangle2D;
+
+import math.Other;
 import pulsar.Main;
 import ui.engine.Point;
 
@@ -58,9 +61,35 @@ public class VectorGraphics {
 		
 		Area a = getArea(v, Main.WIDTH, Main.HEIGHT);
 		
+		String fill = v.getStyle().get("fill");
+		if(!(fill == null || fill.equals("none"))) {
+			
+			String alpha = v.getStyle().get("fill-opacity");
+			if(alpha == null)
+				alpha = "1";
+			
+			graphics.setColor(Other.getColor(fill, alpha));
+			graphics.fill(a);
+			
+		}
 		
-		
-		graphics.draw(a);
+		String stroke = v.getStyle().get("stroke");
+		if(!(stroke == null || stroke.equals("none"))) {
+			
+			String alpha = v.getStyle().get("stroke-opacity");
+			if(alpha == null)
+				alpha = "1";
+			
+			String width = v.getStyle().get("stroke-width");
+			if(width == null)
+				width = "1";
+			BasicStroke style = new BasicStroke(Integer.parseInt(width));
+			
+			graphics.setColor(Other.getColor(stroke, alpha));
+			graphics.setStroke(style);
+			graphics.draw(a);
+			
+		}
 		
 	}
 

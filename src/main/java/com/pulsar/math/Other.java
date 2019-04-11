d9b7e71c6482dc78ed76f3a30c78a5c2582e6f6e
MarshallDBrain <marshalldbrain@gmail.com>
2019-04-11 15:18:27 -0400

    Fixed bug involving lists

--- src/main/java/com/pulsar/math/Other.java
+++ src/main/java/com/pulsar/math/Other.java
@@ -14,25 +14,11 @@ public class Other {
 		return name.substring(i+1);
 	}
 	
-	public static <V> HashMap<String, V> getAllMatchingKeys(HashMap<String, V> map, String name, int i) {
+	public static <T> Map<String, T> getAllMatchingKeys(Map<String, T> map, String name, int i) {
 		
-		HashMap<String, V> newMap = new HashMap<String, V>();
+		Map<String, T> newMap = new HashMap<String, T>();
 		
-		for(Entry<String, V> e: map.entrySet()) {
-			String f = e.getKey();
-			if(f.matches(name + ".*"))
-				newMap.put(f.substring(f.indexOf(f.split("\\.")[i])), e.getValue());
-		}
-		
-		return newMap;
-		
-	}
-
-	public static <T> HashMap<String, List<T>> getAllMatchingKeys(Map<String, List<T>> map, String name, int i) {
-		
-		HashMap<String, List<T>> newMap = new HashMap<String, List<T>>();
-		
-		for(Entry<String, List<T>> e: map.entrySet()) {
+		for(Entry<String, T> e: map.entrySet()) {
 			String f = e.getKey();
 			if(f.matches(name + ".*"))
 				newMap.put(f.substring(f.indexOf(f.split("\\.")[i])), e.getValue());

ecea93759416683e829da790eacc8e31674d56b0
MarshallDBrain <marshalldbrain@gmail.com>
2019-04-11 15:14:49 -0400

    Initial commit

---
+++ src/main/java/com/pulsar/ui/engine/VectorLayer.java
@@ -0,0 +1,18 @@
+package ui.engine;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAnyElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "vector_layer")
+public class VectorLayer {
+
+	@XmlAnyElement(lax = true)
+	private List<Vector> vectors;
+	
+	public List<Vector> getVectors(){
+		return vectors;
+	}
+
+}

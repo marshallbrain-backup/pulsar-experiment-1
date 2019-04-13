9263818c778dc1da782d259e8ed4001f612a36fd
MarshallDBrain <marshalldbrain@gmail.com>
2019-04-12 21:40:25 -0400

    Executes a set of effects based on the action and xml parameters

--- src/main/java/com/pulsar/ui/ActionHandler.java
+++ src/main/java/com/pulsar/ui/ActionHandler.java
@@ -1,18 +1,87 @@
 package ui;
 
+import java.awt.geom.Area;
+import java.util.List;
 import java.util.Map;
 
+import input.Keyboard;
+import input.Mouse;
+import ui.engine.actions.Action;
 import ui.engine.actions.ActionGroup;
+import ui.engine.actions.Click;
+import ui.engine.actions.Effect;
 import ui.engine.vectors.VectorGroup;
+import ui.view.View;
 
 public class ActionHandler {
 	
-	Map<String, VectorGroup> vectorList;
-	Map<String, ActionGroup> actionList;
+	private List<View> viewList;
 	
-	public ActionHandler(Map<String, VectorGroup> vl, Map<String, ActionGroup> al) {
+	private Map<String, VectorGroup> vectorList;
+	private Map<String, ActionGroup> actionList;
+	
+	public ActionHandler(Map<String, VectorGroup> vl, Map<String, ActionGroup> al, List<View> v) {
 		vectorList = vl;
 		actionList = al;
+		viewList = v;
+	}
+
+	public boolean performAction(Mouse m, Keyboard k, String path, Area area) {
+		
+		ActionGroup action = getActionGroup(path);
+		
+		if(action == null) {
+			return false;
+		}
+		
+		for(Action a: action.getActions()) {
+			if(a.didAction(m, k, area)) {
+				for(Effect e: a.getEffect()) {
+					performEffect(e);
+				}
+				return true;
+			}
+		}
+		
+		return false;
+		
+	}
+	
+	private ActionGroup getActionGroup(String path) {
+		
+		while(path.length() != 0) {
+			
+			ActionGroup a = actionList.get(path);
+			if(a != null) {
+				return a;
+			}
+			
+			try {
+				path = path.substring(0, path.lastIndexOf("."));
+			} catch(IndexOutOfBoundsException e) {
+				path = "";
+			}
+			
+		}
+		
+		return null;
+		
+	}
+	
+	private void performEffect(Effect e) {
+		
+		String effect = e.getEffect();
+		
+		switch(e.getType()) {
+			case "open":
+				System.out.println(effect);
+				break;
+			default:
+				System.out.println("Unrecognized class " + e.getType());
+				System.out.println("You should not see this");
+				System.out.println();
+		}
+		
 	}
 	
 }

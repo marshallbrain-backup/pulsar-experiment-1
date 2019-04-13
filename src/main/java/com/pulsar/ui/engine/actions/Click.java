98a9e8a2b54af9e00c34d90c448cd2a0bfa8cf2b
MarshallDBrain <marshalldbrain@gmail.com>
2019-04-12 21:41:46 -0400

    Added functionality to didAction

--- src/main/java/com/pulsar/ui/engine/actions/Click.java
+++ src/main/java/com/pulsar/ui/engine/actions/Click.java
@@ -1,5 +1,6 @@
 package ui.engine.actions;
 
+import java.awt.geom.Area;
 import java.util.List;
 
 import javax.xml.bind.annotation.XmlAnyElement;
@@ -7,6 +8,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 import input.Keyboard;
 import input.Mouse;
+import ui.engine.Point;
 import ui.engine.actions.Action;
 
 @XmlRootElement(name = "click")
@@ -19,9 +21,15 @@ public class Click implements Action {
 	public List<Effect> getEffect(){
 		return effect;
 	}
-	
+
 	@Override
-	public boolean didAction(Mouse m, Keyboard k) {
+	public boolean didAction(Mouse m, Keyboard k, Area area) {
+		if(!effect.isEmpty() && m.buttonClicked(1)) {
+			Point mp = new Point(m.getPosition());
+			if(area.contains(mp.getX(), mp.getY())) {
+				return true;
+			}
+		}
 		return false;
 	}
 	

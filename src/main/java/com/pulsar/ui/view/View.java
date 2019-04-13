2bfeb6ff80685751a9ef82fe05c96129d92d4981
MarshallDBrain <marshalldbrain@gmail.com>
2019-04-13 10:22:12 -0400

    Sets active vectors and actions based on path given
    - Changed from interface to class

--- src/main/java/com/pulsar/ui/view/View.java
+++ src/main/java/com/pulsar/ui/view/View.java
@@ -1,5 +1,43 @@
 package ui.view;
 
-public interface View {
+import java.util.List;
+import java.util.Map;
+
+import ui.engine.actions.Action;
+import ui.engine.actions.ActionGroup;
+import ui.engine.vectors.Vector;
+import ui.engine.vectors.VectorGroup;
+
+public class View {
+	
+	private static Map<String, VectorGroup> vectorList;
+	private static Map<String, ActionGroup> actionList;
+	
+	private List<Vector> activeVectors;
+	private List<Action> activeActions;
+
+	public View(String action) {
+		
+		int s = action.indexOf(";");
+		
+		String par = action.substring(s+1);
+		String key = action.substring(0, s);
+		
+		try {
+			activeVectors = vectorList.get(key).getVectors();
+			activeActions = actionList.get(key).getActions();
+		} catch (NullPointerException e) {
+		}
+		
+		if(activeVectors == null) {
+			throw new NullPointerException();
+		}
+		
+	}
+
+	public static void initGroups(Map<String, VectorGroup> vl, Map<String, ActionGroup> al) {
+		vectorList = vl;
+		actionList = al;
+	}
 
 }

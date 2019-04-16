67ea2d695e21172aeb92f72d19da381acaea79fc
MarshallDBrain <marshalldbrain@gmail.com>
2019-04-16 15:14:47 -0400

    Implemented parameter handling

--- src/main/java/com/pulsar/ui/engine/vectors/VectorGroup.java
+++ src/main/java/com/pulsar/ui/engine/vectors/VectorGroup.java
@@ -1,9 +1,12 @@
 package ui.engine.vectors;
 
 import java.util.List;
+import java.util.Map;
 
+import javax.xml.bind.annotation.XmlAnyAttribute;
 import javax.xml.bind.annotation.XmlAnyElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.namespace.QName;
 
 @XmlRootElement(name = "vector_layer")
 public class VectorGroup {
@@ -11,8 +14,24 @@ public class VectorGroup {
 	@XmlAnyElement(lax = true)
 	private List<Vector> vectors;
 	
+	@XmlAnyAttribute
+	private Map<QName, Object> parameters;
+	
 	public List<Vector> getVectors(){
 		return vectors;
+	}
+	
+	public void propegateParameters() {
+		for(Vector a: vectors) {
+			a.assingParamerters(parameters);
+		}
+	}
+	
+	public void assingParameters(Object... p) {
+		Object[] s = parameters.keySet().toArray();
+		for(int i = 0; i < p.length; i++) {
+			parameters.put((QName) s[i], p[i]);
+		}
 	}
 
 }

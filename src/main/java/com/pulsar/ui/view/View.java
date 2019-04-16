44f7e0e44c75f0a48776b14bd5b36de8bf3849e6
MarshallDBrain <marshalldbrain@gmail.com>
2019-04-16 09:32:58 -0400

    Moved the initialization of vectors to a separate method

--- src/main/java/com/pulsar/ui/view/View.java
+++ src/main/java/com/pulsar/ui/view/View.java
@@ -7,6 +7,7 @@ import ui.engine.Point;
 import ui.engine.VectorGraphics;
 import ui.engine.actions.Action;
 import ui.engine.actions.ActionGroup;
+import ui.engine.vectors.LinkVector;
 import ui.engine.vectors.Vector;
 import ui.engine.vectors.VectorGroup;
 
@@ -25,14 +26,41 @@ public class View {
 		String par = action.substring(s+1);
 		String key = action.substring(0, s);
 		
-		try {
-			activeVectors = vectorList.get(key).getVectors();
-			activeActions = actionList.get(key).getActions();
-		} catch (NullPointerException e) {
+		initVectors(key);
+		
+	}
+	
+	private void initVectors(String key) {
+		
+		key = key.replaceAll("\\s+","");
+		
+		System.out.println("Loading vectors from: " + key);
+		
+		if (activeVectors == null) {
+			try {
+				activeVectors = vectorList.get(key).getVectors();
+				activeActions = actionList.get(key).getActions();
+			} catch (NullPointerException e) {
+			}
+			
+			if(activeVectors == null) {
+				throw new NullPointerException();
+			}
+		} else {
+			try {
+				activeVectors.addAll(vectorList.get(key).getVectors());
+				activeActions.addAll(actionList.get(key).getActions());
+			} catch (NullPointerException e) {
+			}
 		}
 		
-		if(activeVectors == null) {
-			throw new NullPointerException();
+		for(int i = 0; i < activeVectors.size(); i++) {
+			if(activeVectors.get(i).getType().equals("link vector")) {
+				LinkVector link = (LinkVector) activeVectors.get(i);
+				activeVectors.remove(i);
+				i--;
+				initVectors(link.getLink());
+			}
 		}
 		
 	}

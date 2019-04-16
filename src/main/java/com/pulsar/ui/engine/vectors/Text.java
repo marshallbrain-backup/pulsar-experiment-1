f6e725c9c33e832e290f84a499b9c1cc52e94a22
MarshallDBrain <marshalldbrain@gmail.com>
2019-04-16 12:54:40 -0400

    Initial commit

---
+++ src/main/java/com/pulsar/ui/engine/vectors/Text.java
@@ -0,0 +1,77 @@
+package ui.engine.vectors;
+
+import java.awt.Shape;
+import java.util.Map;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import ui.engine.Point;
+
+@XmlRootElement(name = "text")
+public class Text implements Vector {
+
+	@XmlAttribute(name = "pading_x")
+	private int padingX;
+	@XmlAttribute(name = "pading_y")
+	private int padingY;
+
+	@XmlAttribute(name = "style")
+	private String styleString;
+	@XmlElement
+	private String text;
+	
+	private Map<String, Object> parameters;
+
+	@Override
+	public String getType() {
+		return "text";
+	}
+
+	@Override
+	public String getStyleString() {
+		return null;
+	}
+
+	@Override
+	public Shape getShape() {
+		System.out.println("!");
+		return null;
+	}
+
+	@Override
+	public Map<String, String> getStyle() {
+		return null;
+	}
+
+	@Override
+	public void move(Point offset) {
+	}
+
+	@Override
+	public void transform(double offset) {
+	}
+
+	@Override
+	public void normalize(long screenSize, int screenWidth, int minSize) {
+	}
+
+	@Override
+	public void setStyle(Map<String, String> s) {
+	}
+	
+	@Override
+	public Object clone() {
+		
+		try {
+			return super.clone();
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+		
+	}
+	
+}

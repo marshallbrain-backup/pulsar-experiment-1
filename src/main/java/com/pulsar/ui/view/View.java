50bed054839c8e65a73b4da3606bcbc5edb27bb0
MarshallDBrain <marshalldbrain@gmail.com>
2019-04-19 18:28:56 -0400

    Removed stuff that got moved to different classes

--- src/main/java/com/pulsar/ui/view/View.java
+++ src/main/java/com/pulsar/ui/view/View.java
@@ -1,6 +1,7 @@
 package ui.view;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -18,13 +19,10 @@ public class View {
 	private static Map<String, VectorGroup> vectorList;
 	private static Map<String, ActionGroup> actionList;
 	
-	private List<Vector> activeVectors;
-	private List<Action> activeActions;
+	private VectorGroup activeVectors;
+	private ActionGroup activeActions;
 
 	public View(Object[] action) {
-		
-		activeVectors = new ArrayList<Vector>();
-		activeActions = new ArrayList<Action>();
 		
 		initVectors(action);
 		
@@ -32,34 +30,17 @@ public class View {
 	
 	private void initVectors(Object[] action) {
 		
-		VectorGroup av = null;
-		ActionGroup aa = null;
-		
 		String key = (String) action[0];
 		
 		System.out.println("Loading vectors from: " + key);
 
 		try {
-			av = vectorList.get(key);
-			aa = actionList.get(key);
-			activeVectors.addAll(av.getVectors());
-			activeActions.addAll(aa.getActions());
+			activeVectors = vectorList.get(key).clone();
+			activeActions = actionList.get(key);
 		} catch (NullPointerException e) {
 		}
 		
-		av.assingParameters(action);
-		
-		List<Vector> v = av.getVectors();
-		
-		for(int i = 0; i < v.size(); i++) {
-			switch(v.get(i).getType()) {
-				case "link vector":
-					LinkVector link = (LinkVector) v.get(i);
-					activeVectors.remove(link);
-					initVectors(link.getLink());
-					break;
-			}
-		}
+		activeVectors.assingParameters(Arrays.copyOfRange(action, 1, action.length));
 		
 	}
 
@@ -69,13 +50,8 @@ public class View {
 	}
 
 	public void render(VectorGraphics vg) {
-		
 		vg.translationSet(new Point(150, 100));
-		
-		for(Vector v: activeVectors) {
-			v.draw(vg);
-		}
-		
+		activeVectors.draw(vg);
 	}
 
 }

b1d79cb363207a39036a8a9ff93d17227672a2aa
MarshallDBrain <marshalldbrain@gmail.com>
2019-04-20 14:32:55 -0400

    Initial commit

---
+++ src/main/java/com/pulsar/ui/map/BodyUi.java
@@ -0,0 +1,85 @@
+package ui.map;
+
+import java.awt.geom.Area;
+import java.util.List;
+import java.util.Map;
+
+import bodys.Body;
+import pulsar.Main;
+import ui.engine.EntrySet;
+import ui.engine.Point;
+import ui.engine.VectorGraphics;
+import ui.engine.scripts.ScriptGroup;
+import ui.engine.vectors.Vector;
+import ui.engine.vectors.VectorGroup;
+
+public class BodyUi {
+	
+	private Body body;
+	private Area visibleArea;
+	private VectorGroup vectors;
+	private ScriptGroup scrip;
+	
+	private Map<String, VectorGroup> modifierVectors;
+	
+	public BodyUi(Body b, Map<String, VectorGroup> v, Map<String, VectorGroup> m, ScriptGroup s) {
+		
+		vectors = getVectors(b.getPath(), v);
+		
+		modifierVectors = m;
+		scrip = s;
+		body = b;
+	}
+	
+	public boolean action() {
+		return false;
+	}
+	
+	public void render(VectorGraphics vg, Long zoom) {
+		
+		if(body.getParent() != null) {
+			for(Vector v: modifierVectors.get("orbit").getVectors()) {
+				Vector vt = (Vector) v.clone();
+				vt.move(new Point(body.getParent().getX(), body.getParent().getY()));
+				vt.transform(new Point(body.getDistance(), 0));
+				vt.normalize(zoom, Main.WIDTH, 8);
+				vt.draw(vg);
+			}
+		}
+		
+		vg.startLogArea();
+		
+		for(Vector v: vectors.getVectors()) {
+			Vector vt = (Vector) v.clone();
+			vt.move(new Point(body.getX(), body.getY()));
+			vt.transform(new Point(body.getRadius(), 0));
+			vt.normalize(zoom, Main.WIDTH, 8);
+			vt.draw(vg);
+		}
+		
+		if(body.getColony() != null) {
+			for(Vector v: modifierVectors.get("colony").getVectors()) {
+				Vector vt = (Vector) v.clone();
+				vt.move(new Point(body.getX(), body.getY()));
+				vt.transform(new Point(body.getRadius(), 0));
+				vt.normalize(zoom, Main.WIDTH, 8);
+				vt.draw(vg);
+			}
+		}
+		
+		Area a = vg.stopLogArea();
+		if(a != null) {
+			visibleArea = a;
+		}
+		
+	}
+	
+	private VectorGroup getVectors(String id, Map<String, VectorGroup> v){
+		try {
+			return v.getOrDefault(id, getVectors(id.substring(0, id.lastIndexOf(".")), v));
+		} catch(IndexOutOfBoundsException e) {
+			return null;
+		}
+	}
+	
+}

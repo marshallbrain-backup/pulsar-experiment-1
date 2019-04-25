06af94ce821219038a09af15c08f2b645bd1e7a6
MarshallDBrain <marshalldbrain@gmail.com>
2019-04-25 16:46:41 -0400

    Added more tokens

--- src/main/java/com/pulsar/ui/engine/scripts/ScriptGroup.java
+++ src/main/java/com/pulsar/ui/engine/scripts/ScriptGroup.java
@@ -36,10 +36,21 @@ public class ScriptGroup {
 	
 	private Token[] getExprs() {
 		return new Token[]{
-				new Token("\\p{Space}", TokenType.NONE),
-				new Token("[#]", TokenType.NONE),
-				new Token("[;]", TokenType.NEWLINE),
-				new Token("fun", TokenType.ID.FUNCTION)
+				new Token("^\\p{Space}", TokenType.NONE),
+				new Token("^[#]", TokenType.NONE),
+				new Token("^[;]", TokenType.NEWLINE),
+				new Token("^fun", TokenType.KEY.FUN),
+				new Token("^\\(", TokenType.OP.OPAR),
+				new Token("^\\)", TokenType.OP.CPAR),
+				new Token("^\\{", TokenType.OP.OBRACE),
+				new Token("^\\}", TokenType.OP.CBRACE),
+				new Token("^,", TokenType.OP.COMA),
+				new Token("^\"\\p{ASCII}*\"", TokenType.LIT.STRING),
+				new Token("^[a-zA-Z][a-zA-Z_0-9]*(?=\\.)", TokenType.ID),
+				new Token("^[a-zA-Z][a-zA-Z_0-9]*(?=\\()", TokenType.FUN),
+				new Token("^[a-zA-Z][a-zA-Z_0-9]*", TokenType.VAR),
+				new Token("^[0-9]", TokenType.LIT.INT),
+				new Token("^\\.", TokenType.OP.DOT),
 		};
 	}
 

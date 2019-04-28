9ff933088a816ab77cdd50489c6522bb854b84a5
MarshallDBrain <marshalldbrain@gmail.com>
2019-04-28 18:29:26 -0400

    Added parent node

--- src/main/java/com/pulsar/ui/engine/scripts/ast/NodeCallFun.java
+++ src/main/java/com/pulsar/ui/engine/scripts/ast/NodeCallFun.java
@@ -5,10 +5,12 @@ public class NodeCallFun implements NodeCall {
 	
 	public final String name;
 	public final Node[] paramaters;
+	public final Node parent;
 	
-	public NodeCallFun(String n, Node[] p) {
-		name = n;
+	public NodeCallFun(Node n, Node[] p, Node pa) {
+		name = ((NodeBasic) n).type.ex;
 		paramaters = p;
+		parent = pa;
 	}
 	
 }

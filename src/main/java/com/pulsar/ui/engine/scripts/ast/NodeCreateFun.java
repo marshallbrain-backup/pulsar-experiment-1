7f81caa29be9843f8bfa2b150fee32a77f33f30c
MarshallDBrain <marshalldbrain@gmail.com>
2019-04-28 21:27:55 -0400

    Changed Node[] paramaters to NodeCallVar[] paramaters

--- src/main/java/com/pulsar/ui/engine/scripts/ast/NodeCreateFun.java
+++ src/main/java/com/pulsar/ui/engine/scripts/ast/NodeCreateFun.java
@@ -4,14 +4,16 @@ package ui.engine.scripts.ast;
 public class NodeCreateFun implements NodeCreate {
 	
 	public final String name;
-	public final Node[] paramaters;
+	public final NodeCallVar[] paramaters;
 	public final Node[] body;
 	
 	public NodeCreateFun(String n, NodeList p, NodeList b) {
 		name = n;
 		
-		paramaters = new Node[p.nodeList.size()];
-		p.nodeList.toArray(paramaters);
+		paramaters = new NodeCallVar[p.nodeList.size()];
+		for(int i = 0; i < p.nodeList.size(); i++) {
+			paramaters[i] = (NodeCallVar) p.nodeList.get(i);
+		}
 		
 		body = new Node[b.nodeList.size()];
 		b.nodeList.toArray(body);

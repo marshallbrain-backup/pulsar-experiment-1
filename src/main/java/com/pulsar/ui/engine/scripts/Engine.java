2d76436d7d791b766dcb6d91d86fb276defcd8ea
MarshallDBrain <marshalldbrain@gmail.com>
2019-04-29 23:04:44 -0400

    openView now loads the target view

--- src/main/java/com/pulsar/ui/engine/scripts/Engine.java
+++ src/main/java/com/pulsar/ui/engine/scripts/Engine.java
@@ -1,11 +1,36 @@
 package ui.engine.scripts;
 
+import java.util.List;
+import java.util.Map;
+
 import bodys.Body;
+import ui.engine.vectors.VectorGroup;
+import ui.view.View;
 
 public class Engine {
 	
-	public void openView(String s, Object o) {
+	private List<View> views;
+	
+	private Map<String, VectorGroup> vectorList;
+	private Map<String, ScriptGroup> scriptList;
+	
+	public Engine(List<View> v, Map<String, VectorGroup> vl, Map<String, ScriptGroup> sl) {
+		
+		views = v;
+		vectorList = vl;
+		scriptList = sl;
+		
+	}
+
+	public void openView(String s, Object... o) {
 		System.out.println("view opened - " + s);
+		
+		VectorGroup vectors = vectorList.get(s);
+		ScriptGroup scripts = scriptList.get(s);
+		
+		views.clear();
+		views.add(new View(vectors, scripts, o));
+		
 	}
 	
 }

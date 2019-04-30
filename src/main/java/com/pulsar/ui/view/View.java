1bd7eba465ab29061c334d99993627bc3c55f05e
MarshallDBrain <marshalldbrain@gmail.com>
2019-04-29 23:06:15 -0400

    Calls the function onCreate from the script file on load

--- src/main/java/com/pulsar/ui/view/View.java
+++ src/main/java/com/pulsar/ui/view/View.java
@@ -5,8 +5,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import ui.Ui;
 import ui.engine.Point;
 import ui.engine.VectorGraphics;
+import ui.engine.scripts.ScriptGroup;
 import ui.engine.vectors.LinkVector;
 import ui.engine.vectors.TextRegion;
 import ui.engine.vectors.Vector;
@@ -14,33 +16,18 @@ import ui.engine.vectors.VectorGroup;
 
 public class View {
 	
-	private static Map<String, VectorGroup> vectorList;
-	
 	private VectorGroup activeVectors;
+	private ScriptGroup activeScripts;
 
-	public View(Object[] action) {
+	public View(VectorGroup av, ScriptGroup as, Object... action) {
 		
-		initVectors(action);
+		activeVectors = av;
+		activeScripts = as;
 		
-	}
-	
-	private void initVectors(Object[] action) {
-		
-		String key = (String) action[0];
-		
-		System.out.println("Loading vectors from: " + key);
-
-		try {
-			activeVectors = vectorList.get(key).clone();
-		} catch (NullPointerException e) {
+		if(activeScripts != null) {
+			activeScripts.callFunction("onCreate", action);
 		}
 		
-		activeVectors.assingParameters(Arrays.copyOfRange(action, 1, action.length));
-		
-	}
-
-	public static void initGroups(Map<String, VectorGroup> vl) {
-		vectorList = vl;
 	}
 
 	public void render(VectorGraphics vg) {

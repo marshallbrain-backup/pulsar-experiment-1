e4d8de51dc05cd9e70c173829045c5fa1c050ccb
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-07 20:50:52 -0400

    Added name of the variable

--- src/main/java/com/pulsar/ui/engine/scripts/ast/NodeAssignVar.java
+++ src/main/java/com/pulsar/ui/engine/scripts/ast/NodeAssignVar.java
@@ -5,10 +5,12 @@ public class NodeAssignVar implements Node {
 	
 	public final Node target;
 	public final Node value;
+	public final String name;
 	
 	public NodeAssignVar(Node t, Node v) {
 		target = t;
 		value = v;
+		name = ((NodeCreateVar) t).name;
 	}
 	
 }

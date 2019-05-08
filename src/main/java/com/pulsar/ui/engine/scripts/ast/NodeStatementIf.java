133782849187e735d8af5f55cc2ae1f65191e180
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-07 21:31:28 -0400

    Added elseCode variable

--- src/main/java/com/pulsar/ui/engine/scripts/ast/NodeStatementIf.java
+++ src/main/java/com/pulsar/ui/engine/scripts/ast/NodeStatementIf.java
@@ -5,10 +5,12 @@ public class NodeStatementIf implements NodeStatement {
 
 	public final Node condition;
 	public final Node code;
+	public final Node elseCode;
 	
-	public NodeStatementIf(Node p, Node c) {
+	public NodeStatementIf(Node p, Node c, Node e) {
 		condition = p;
 		code = c;
+		elseCode = e;
 	}
 	
 }

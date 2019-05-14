c7537876d0dc9a6653485adc27f34ca615676a7e
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-13 21:10:37 -0400

    Added district initialization

--- src/main/java/com/pulsar/species/colony/Colony.java
+++ src/main/java/com/pulsar/species/colony/Colony.java
@@ -1,24 +1,48 @@
 package species.colony;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import bodys.Body;
 import species.Species;
+import species.colony.build.District;
 
 public class Colony {
 	
 	private Body body;
 	
-	/**
-	 * initalizes  colony
-	 * 
-	 * @param planet the planet that the colony is on
-	 * @param species the species that the colony is under controle by, relevent for species boneses
-	 * @param gamefile the gamefile containing district info
-	 */
+	private District[] districts;
+	
 	public Colony(Body b, Species s) {
 		
 		body = b;
 		
 		body.setColony(this); 
+		
+		init();
+		
+	}
+	
+	private void init() {
+		
+		initDistricts();
+		
+	}
+
+	private void initDistricts() {
+		
+		districts = new District[4];
+		
+		List<String> potential = District.getPotentialDistricts(body.getTags());
+		
+		for(int i = 0; i < districts.length; i++) {
+			if(i < potential.size()) {
+				districts[i] = new District(potential.get(i), body.getTags());
+			} else {
+				districts[i] = new District(body.getTags());
+			}
+		}
 		
 	}
 
